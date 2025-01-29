a83e9107d8da231802e43ef24687fdb4954150f7
dibucam <dibucam9@gmail.com>
2025-01-29 16:25:23 -0500

    fecha y hora

--- resources/views/admin/documento/lista.blade.php
+++ resources/views/admin/documento/lista.blade.php
@@ -37,6 +37,7 @@
                     <th> DNI </th>
                     <th> Modalidad</th>
                     <th> ACTIVO </th>
+                    <th> ULT HORA</th>
                     <th>Opciones </th>
                 </tr>
                 </thead>
@@ -58,6 +59,7 @@
                                 <span class="m-badge m-badge--success"> APROBADO </span>
                             @endif
                         </td>
+                        <td> {{ $item->fecha }}</td>
                         <td>
 
                             <a href="evaluar-postulante/{!! $item->dni !!}" class="btn btn-outline-info m-btn m-btn--custom m-btn--outline-2x" >
