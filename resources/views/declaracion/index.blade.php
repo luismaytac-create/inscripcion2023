a1f4ce400b91d3ab12d23b372f41cd56d165475b
PowerST2 <diegostd99@gmail.com>
2026-01-06 19:45:06 +0000

    Cambiando la declaración jurada

--- resources/views/declaracion/index.blade.php
+++ resources/views/declaracion/index.blade.php
@@ -48,9 +48,9 @@
                    <h2>1. Descarga la declaración jurada:</h2>
                         <a target="_blank"
                           @if($postulante->idmodalidad == 16 )
-                          href="{{ asset("DECLARACION_JURADA_IEN_26-1.pdf")  }}"
+                          href="{{ asset("DECLARACION_JURADA_26-1.pdf")  }}"
                           @else
-                          href="{{ asset("DECLARACION_JURADA_IEN_26-1.pdf")  }}"
+                          href="{{ asset("DECLARACION_JURADA_26-1.pdf")  }}"
                                   @endif
 
                           class="btn btn-success m-btn  m-btn--icon">
